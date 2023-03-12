916f155cc496a7b2d42b4e27aa857a9cd294a15a
Mohammad Aoulad Hossain <121309917+MD-Aoulad@users.noreply.github.com>
2023-03-12 21:44:16 +0100

    Update excel_marge.py

--- excel_marge.py
+++ excel_marge.py
@@ -1,16 +1,31 @@
 import pandas as pd
 import os
 
-path = 'C:/Users/ekra5/Desktop/python/test python project/amazon/'
+# Specify the directory containing the input files. Please use "/" instead of "\". It took me quite some time to find the issue.
+input_dir = '/path/to/input/directory/'
 
-files = os.listdir(path)
+# Get a list of all files in the directory
+files = os.listdir(input_dir)
+
+# Initialize an empty list to store the dataframes
 dfs = []
+
+# Loop through each file in the directory
 for file in files:
+    # Check if the file is an Excel file
     if file.endswith('.xlsx'):
-        df = pd.read_excel(path + file)
+        # Read the Excel file as a dataframe and append it to the list
+        df = pd.read_excel(input_dir + file)
         dfs.append(df)
 
-output_file = 'C:/Users/ekra5/Desktop/python/test python project/amazon/merged_excel_file.xlsx'
+# Specify the path and filename of the output file
+output_file = '/path/to/output/file/merged_excel_file.xlsx'
+
+# Concatenate all dataframes into a single dataframe
 merged_df = pd.concat(dfs)
+
+# Write the merged dataframe to an Excel file
 merged_df.to_excel(output_file, index=False)
-print("File created at:", output_file)
+
+# Print a message indicating that the file has been created and its location
+print("Merged file created at:", output_file)
